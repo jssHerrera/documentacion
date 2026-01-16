e543a3e3f7143f4595232551d6bf541e10b3260e
Denis Herrera Espiritu <jsherr.6@gmail.com>
2026-01-16 12:53:04 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'API Konnect',
 			sidebar: [
 				{
 					label: 'Guides',
